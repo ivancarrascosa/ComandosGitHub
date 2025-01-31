b2f11fdcad899149dd9cc4f21b474538cf42e36f
ruben-carrasco <ruben.carrasco@iesnervion.es>
2025-01-31 12:43:04 +0100

    Cambios de Ruben

--- src/paquete/clasePrueba.java
+++ src/paquete/clasePrueba.java
@@ -4,6 +4,8 @@ public class clasePrueba {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Linea añadida por Ruben sin hacer un Pull, en la misma clase
+			System.out.println("Cambio");
 
 	}
 
